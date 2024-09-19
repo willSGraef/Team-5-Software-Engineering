627872f720ef3117014d8ac30ece1995fa94bd83
John Guresky <jguresky@uark.edu>
2024-09-19 16:03:58 -0500

    Added SplashScreen.java, added rm for generated files

--- photon/build.bash
+++ photon/build.bash
@@ -1,8 +1,12 @@
-#!/bin.bash
-echo Building PHOTON Project for MACOS
+#!/bin/bash
+echo Building PHOTON Project
 
-javac Game.java View.java Controller.java Model.java Player.java
-javac -d . Game.java View.java Controller.java Model.java Player.java
+javac Game.java View.java Controller.java Model.java Player.java SplashScreen.java
+javac -d . Game.java View.java Controller.java Model.java Player.java SplashScreen.java
 java photon/Game
 
-echo Build finished
+echo removing generated files
+
+rm Game.class View.class Controller.class Model.class Player.class SplashScreen.class
+rm photon/Game.class photon/View.class photon/Controller.class photon/Model.class photon/Player.class photon/SplashScreen.class
+rmdir photon
