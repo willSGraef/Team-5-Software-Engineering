3d94e80d5bf7fc5aaec1271187f3492d98f9d133
willSGraef <willgraef@gmail.com>
2024-09-19 16:04:12 -0500

    Revert "Update View.java"
    This reverts commit cc5c06d6d49322d60ae3ded722ea5a34ff32b7cc.

--- photon/View.java
+++ photon/View.java
@@ -1,39 +1,43 @@
 package photon;
-
 import java.awt.Color;
 import java.awt.Graphics;
-
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class View extends JPanel {
-    private Model model;
-    private Game game;
+public class View extends JPanel{
 
-    public View(Controller c, Model m, Game g) {
-        model = m;
-        c.setView(this);
-
-        // Example: Create JTextField components to represent teams
-        for (int i = 0; i < 20; i++) {
-            JTextField tempEntry = new JTextField();
-            tempEntry.setBounds(100, 20 * i + 50, 150, 20);
-            g.add(tempEntry);  
-        }
-
-        for (int i = 0; i < 20; i++) {
-            JTextField tempEntry = new JTextField();
-            tempEntry.setBounds(300, 20 * i + 50, 150, 20);
-            g.add(tempEntry);  
-        }
-
-        g.setLayout(null);
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(new Color(35, 5, 48)); // Example background color
-        // Draw other UI elements as needed
-    }
+	private Model model;
+	private Game game;
+    
+	public View(Controller c, Model m, Game g)
+	{
+		model = m;
+		c.setView(this);
+		
+		//team 1
+		for (int i = 0; i < 20; i++)
+		{
+			JTextField tempEntry = new JTextField();  
+			tempEntry.setBounds(100, 20*i+50, 150, 20); 
+	    	g.add(tempEntry);  
+		}
+		// team 2
+		for (int i = 0; i < 20; i++)
+		{
+			JTextField tempEntry = new JTextField();  
+			tempEntry.setBounds(300, 20*i+50, 150, 20); 
+	    	g.add(tempEntry);  
+		}
+		
+    	//g.setSize(300,300);  
+    	g.setLayout(null);  
+    	//team1Entries.setVisible(true);
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		g.setColor(new Color(35, 5, 48));
+		//g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		
+	}
 }
