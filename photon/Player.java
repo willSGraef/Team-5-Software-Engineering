d605b0f7afaf4147f635d20608e13f477735866b
willSGraef <willgraef@gmail.com>
2024-09-19 16:04:21 -0500

    Revert "Update Player.java"
    This reverts commit 289ff8414cf03d449632c03a28de9289c2e313e7.

--- photon/Player.java
+++ photon/Player.java
@@ -1,14 +1,14 @@
 package photon;
 
 public class Player {
-    private int id;
-    private String name;
 
-    public Player(int entryID, String entryName) {
-        id = entryID;
-        name = entryName;
-    }
-
-    // Getter and Setter methods for player attributes can be added
+	private int id;
+	private String name;
+	
+	public Player(int entryID, String entryName) 
+	{
+		id = entryID;
+		name = entryName;
+		
+	}
 }
-
