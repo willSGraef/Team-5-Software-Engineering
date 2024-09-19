d746384909bc7c7320a1c6d0f6c2578e0f5c7d3b
ZizhengLiu2719 <zl024@email.uark.edu>
2024-09-19 15:58:18 -0500

    Update Model.java

--- photon/Model.java
+++ photon/Model.java
@@ -1,11 +1,14 @@
 package photon;
+
 import java.util.ArrayList;
 
 public class Model {
-	private ArrayList<Player> database;
-	
-	public Model()
-	{
-		database = new ArrayList<Player>();
-	}
+    private ArrayList<Player> database;
+
+    public Model() {
+        database = new ArrayList<>();
+    }
+
+    // Additional methods to manage player data can be added here
 }
+
