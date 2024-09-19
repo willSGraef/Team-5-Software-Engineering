b1e17304bd7fa4ee2769b0003ebcbc529a886836
ZizhengLiu2719 <zl024@email.uark.edu>
2024-09-19 15:58:15 -0500

    Update Game.java

--- photon/Game.java
+++ photon/Game.java
@@ -1,50 +1,59 @@
 package photon;
 
-import javax.swing.JFrame;
 import java.awt.Toolkit;
 
-public class Game extends JFrame{
-	private Model model = new Model();
-	private Controller controller = new Controller(model);
-	private View view = new View(controller, model, this);
-	
-	public Game()
-	{
-		this.setTitle("Photon UI Demo");
-		this.setSize(614, 638);
-		this.setFocusable(true);
-		this.getContentPane().add(view);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
-		view.addMouseMotionListener(controller);
-		view.addMouseListener(controller);
-		this.addKeyListener(controller);
-	}
-	
-	public static void main(String[] args) {
-		//System.out.println("Hello!");
-		Game g = new Game();
-		g.run();
-	}
-	
-	public void run()
-	{
-		while(true)
-		{
-			//controller.update();
-			//model.update();
-			view.repaint(); // This will indirectly call View.paintComponent
-			Toolkit.getDefaultToolkit().sync(); // Updates screen
+import javax.swing.JFrame;
 
-			// Go to sleep for 40 milliseconds
-			try
-			{
-				Thread.sleep(40);
-			} catch(Exception e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-		}
-	}
+public class Game extends JFrame {
+    private Model model = new Model();
+    private Controller controller = new Controller(model);
+    private View view = new View(controller, model, this);
+    
+    public Game() {
+        this.setTitle("Photon UI Demo");
+        this.setSize(614, 638);
+        this.setFocusable(true);
+        this.getContentPane().add(view);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        view.addMouseMotionListener(controller);
+        view.addMouseListener(controller);
+        this.addKeyListener(controller);
+    }
 
+    public static void main(String[] args) {
+        // Show splash screen before launching the game
+        SplashScreen splash = new SplashScreen();
+        splash.showSplash();
+
+        // Simulate loading process (e.g., 3 seconds)
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Close the splash screen
+        splash.closeSplash();
+
+        // Launch the game
+        Game g = new Game();
+        g.run();
+    }
+    
+    public void run() {
+        while (true) {
+            view.repaint(); // This will indirectly call View.paintComponent
+            Toolkit.getDefaultToolkit().sync(); // Updates screen
+
+            // Go to sleep for 40 milliseconds (controls game speed)
+            try {
+                Thread.sleep(40);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
 }
+
