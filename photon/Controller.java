b25ed1db35f43b6d6ad79a4c5198fdc79f7892be
Azlon Thomas <azlon.r.thomas@gmail.com>
2024-10-20 19:42:28 -0500

    Reworked player entry screen

--- photon/Controller.java
+++ photon/Controller.java
@@ -39,17 +39,17 @@ public class Controller implements ActionListener, KeyListener, CountDownListene
 	public void keyPressed(KeyEvent e) {
 		System.out.println(e.getKeyCode());
 		switch (e.getKeyCode()) {
-			case KeyEvent.VK_INSERT: // perhaps use a different key?
+			case KeyEvent.VK_F3: // perhaps use a different key?
 				try {
 					model.addPlayer();
 				} catch (Exception e1) {
 					System.out.println("SQL ERROR");
 				}
 				break;
-			case KeyEvent.VK_RIGHT: //perhaps change to a function key
+			case KeyEvent.VK_F2: //perhaps change to a function key
 				model.shiftActiveFieldForward();
 				break;
-			case KeyEvent.VK_LEFT: //perhaps change to a function key
+			case KeyEvent.VK_F1: //perhaps change to a function key
 				model.shiftActiveFieldBackward();
 				break;
 			case KeyEvent.VK_F12:
