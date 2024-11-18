a1a2aee3b95bfd360d807a8a71b6cdb29a2119ab
willSGraef <willgraef@gmail.com>
2024-11-17 18:09:37 -0600

    Final fix for audioplayer hopefully

--- photon/AudioPlayer.java
+++ photon/AudioPlayer.java
@@ -4,6 +4,9 @@ import java.util.Random;
 import java.io.FileInputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Path;
 
 public class AudioPlayer implements Runnable{
 
@@ -18,17 +21,29 @@ public class AudioPlayer implements Runnable{
         String filePath = "game_audio\\Track0" + randomTrackNumber + ".mp3";
         //Load class from jar file, then play
         try {
-            //Specify class name, constructor
-            String className = "net.javazoom.JLayer.Player";
-            Class<?> playerClass = Class.forName(className);
-            Constructor<?> constructor = playerClass.getConstructor();
-            
-            //Dynamically create player object and invoke method
-            FileInputStream audioInput = new FileInputStream(filePath);
-            Object player = constructor.newInstance(audioInput);
-            Thread.sleep(11000);
-            Method method = playerClass.getMethod("play");
-            method.invoke(player);
+             // Path to the JAR file
+            Path jarPath = Path.of("../lib/jlayer-1.0.1.jar");
+
+            // Convert the JAR path to a URL
+            URL jarUrl = jarPath.toUri().toURL();
+
+            // Load the JAR file dynamically
+            try (URLClassLoader loader = new URLClassLoader(new URL[]{jarUrl})) {
+                
+                //Specify class name, constructor
+                String className = "javazoom.jl.player.Player";
+                Class<?> playerClass = loader.loadClass(className);
+                Constructor<?> constructor = playerClass.getConstructor();
+                
+                //Dynamically create player object and invoke method
+                FileInputStream audioInput = new FileInputStream(filePath);
+                Object player = constructor.newInstance(audioInput);
+                Thread.sleep(11000);
+                Method method = playerClass.getMethod("play");
+                method.invoke(player);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
